ef9cc9ffbe609c7bc20a2b36c5551ae3c5f7dcee
David Woodcock <dwoodcock@AXIS-DavidW-MacPro.local>
2017-06-22 12:18:54 -0600

    added files for supercool hello world app

--- Hello World/ViewController.swift
+++ Hello World/ViewController.swift
@@ -10,6 +10,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var background: UIImageView!
+    @IBOutlet weak var titleimage: UIImageView!
+    @IBOutlet weak var welcomebtn: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +23,11 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func welcomepressed(_ sender: Any) {
+        background.isHidden = false
+        titleimage.isHidden = false
+        welcomebtn.isHidden = true
+    }
 
 }
 
